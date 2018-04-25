e7ba3ad2b19e38ca7cfff1f3b42604164cc14d1c
lightningstudios126 <ethan.wong@hotmail.com>
2018-04-25 14:36:08 -0400

    Initial commit

---
+++ src/MainClass.java
@@ -0,0 +1,23 @@
+import processing.core.PApplet;
+
+public class MainClass extends PApplet {
+    public static void main(String[] args) {
+        PApplet.main("MainClass");
+    }
+
+    @Override
+    public void settings() {
+        size(300, 300);
+    }
+
+    @Override
+    public void setup() {
+        fill(120, 50, 240);
+    }
+
+    @Override
+    public void draw() {
+        ellipse(mouseX, mouseY, 30, 30);
+
+    }
+}
