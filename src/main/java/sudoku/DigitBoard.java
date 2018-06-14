377823b34b5eef7d614f51da8ca02217ccbb0d04
lightningstudios126 <ethan.wong@hotmail.com>
2018-06-14 14:35:09 -0400

    add keyboard controls

--- src/main/java/sudoku/DigitBoard.java
+++ src/main/java/sudoku/DigitBoard.java
@@ -12,16 +12,16 @@ public class DigitBoard extends DrawableElement {
     private int[] digits;
 
     public int selectedDigit = 1;
-    
+
     public DigitBoard(PApplet parent) {
         super(parent);
-        
+
         this.parent = parent;
-        
+
         digits = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
     }
-    
-    
+
+
     @Override
     public void update() {
         for (int x = 0; x < 3; x++) {
@@ -29,7 +29,8 @@ public class DigitBoard extends DrawableElement {
                 int cellX = this.position.x + x * (sideLength + spacing);
                 int cellY = this.position.y + y * (sideLength + spacing);
 
-                if ((parent.mouseX > cellX && parent.mouseX < cellX + sideLength) && (parent.mouseY > cellY && parent.mouseY < cellY + sideLength)) {
+                if ((parent.mouseX > cellX && parent.mouseX < cellX + sideLength) &&
+                        (parent.mouseY > cellY && parent.mouseY < cellY + sideLength)) {
                     if (selectedDigit != digits[x + y * 3] && Input.getMouseButton(Input.Button.LEFT, Input.Event.PRESS)) {
                         selectedDigit = digits[x + y * 3];
                     }
@@ -37,36 +38,37 @@ public class DigitBoard extends DrawableElement {
             }
         }
         // use keyboard
+        if (parent.keyPressed && Character.isDigit(parent.key) && parent.key != '0') {
+            selectedDigit = ((int) parent.key - 60 + 12);
+        }
+
+        System.out.println(selectedDigit);
     }
 
     @Override
     public void draw() {
         parent.pushStyle();
         parent.textFont(parent.createFont("Consolas", 50, true));
-    
+
         parent.noStroke();
 
         for (int x = 0; x < 3; x++) {
             for (int y = 0; y < 3; y++) {
                 int cellX = this.position.x + x * (sideLength + spacing);
                 int cellY = this.position.y + y * (sideLength + spacing);
-    
-                if (digits[x + y * 3] == selectedDigit)
-                {
+
+                if (digits[x + y * 3] == selectedDigit) {
                     parent.fill(SolarizedColours.getColour(4));
                     parent.rect(cellX, cellY, sideLength, sideLength);
-                }
-                else
-                {
+                } else {
                     parent.fill(SolarizedColours.getColour(3));
 
                     parent.rect(cellX, cellY, sideLength, sideLength);
                 }
-    
-    
+
+
                 parent.fill(SolarizedColours.getText());
                 parent.text(digits[x + y * 3], cellX + sideLength / 4, cellY + sideLength - (sideLength / 4));
-
             }
         }
         parent.popStyle();
